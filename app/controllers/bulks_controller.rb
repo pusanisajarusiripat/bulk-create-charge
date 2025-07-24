cf9d072adc969a2f306c342b1a5dcd6dc1413044
Pusanisa Jarusiripat <pusanisa.j@PusanisaJ-MacBook-Pro-M1.local>
2025-07-24 15:58:32 +0700

    fix: scan ruby failing

--- app/controllers/bulks_controller.rb
+++ app/controllers/bulks_controller.rb
@@ -1,5 +1,5 @@
 class BulksController < ApplicationController
-  http_basic_authenticate_with name: "user", password: "secret"
+  http_basic_authenticate_with name: ENV["BULK_AUTH_USER"], password: ENV["BULK_AUTH_PASSWORD"]
 
   def index
     # @bulks = Bulk.all
