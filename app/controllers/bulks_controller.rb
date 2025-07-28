9c5be00acfc8493ec378d0aea99c4fb9c208678e
Pusanisa Jarusiripat <pusanisa.j@PusanisaJ-MacBook-Pro-M1.local>
2025-07-28 16:51:58 +0700

    feat: added charges into db

--- app/controllers/bulks_controller.rb
+++ app/controllers/bulks_controller.rb
@@ -17,21 +17,9 @@ class BulksController < ApplicationController
     uploaded_file = params[:bulk][:file]
     @bulk = Bulk.new
 
-    if uploaded_file.nil?
-      @bulk.errors.add(:file, "must be uploaded")
+    if !validate_uploaded_file(uploaded_file, @bulk)
       render :new and return
     end
-
-    unless [ "text/csv", "application/csv", "text/plain" ].include?(uploaded_file.content_type)
-      @bulk.errors.add(:file, "must be a CSV file (e.g., .csv extension, or content type 'text/csv')")
-      render :new and return
-    end
-
-    unless File.extname(uploaded_file.original_filename).downcase == ".csv"
-      @bulk.errors.add(:file, "must have a .csv extension")
-      render :new and return
-    end
-
 
     if @bulk.save
         @bulk.file.attach(uploaded_file)
@@ -45,7 +33,6 @@ class BulksController < ApplicationController
 
         # create charges
         Charge.create_from_csv(@bulk.file, @bulk)
-        # save file info in @bulk
         # render charges.import, starting the import process
 
         @bulk.update(amount_of_charges: row_count)
@@ -55,6 +42,7 @@ class BulksController < ApplicationController
       render :new
     end
   end
+
   def destroy
     @bulk = Bulk.find(params[:id])
     if @bulk.destroy
@@ -68,5 +56,24 @@ class BulksController < ApplicationController
   private
     def bulk_params
       params.require(:bulk).permit(:file)
+    end
+
+    def validate_uploaded_file(uploaded_file, bulk)
+      if uploaded_file.nil?
+        bulk.errors.add(:file, "must be uploaded")
+        return false
+      end
+
+      unless ["text/csv", "application/csv", "text/plain"].include?(uploaded_file.content_type)
+        bulk.errors.add(:file, "must be a CSV file (e.g., .csv extension, or content type 'text/csv')")
+        return false
+      end
+
+      unless File.extname(uploaded_file.original_filename).downcase == ".csv"
+        bulk.errors.add(:file, "must have a .csv extension")
+        return false
+      end
+
+      true
     end
 end
